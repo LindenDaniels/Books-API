cbeec1e80cbae9db0bfdf816b96a7ce935d13f5c
Linden Daniels <lindendaniels@gmail.com>
2019-07-05 14:28:40 -0400

    Attempt to implement google books viewer

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function formatQueryParams(params) {
     .map (key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
+google.books.load();
+
+ function initialize() {
+     console.log(`initialize ran`);
+    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+    let bookISBN = `${responseJson.items[i].volumeInfo.industryIdentifiers[1].identifier}`.val();
+    console.log(bookISBN);
+     viewer.load('ISBN:' + bookISBN);
+    
+   }
+google.books.setOnLoadCallback(initialize);
 
 function displayResults(responseJson, maxResults) {
     console.log(`displayResults ran`);
@@ -17,19 +28,30 @@ function displayResults(responseJson, maxResults) {
     $('#results-list').empty();
     
     for (let i = 0; i < responseJson.items.length & i < maxResults; i++){
-   
+        
         $('#results-list').append(
-          `
-          
+          `<li>
+          <p>${responseJson.items[i].volumeInfo.industryIdentifiers[1].identifier}</p>
           <img src="${responseJson.items[i].volumeInfo.imageLinks.thumbnail} alt="The book">
-          <li><h3>${responseJson.items[i].volumeInfo.title}</h3>
+          <h3>${responseJson.items[i].volumeInfo.title}</h3>
           <h2>${responseJson.items[i].volumeInfo.authors}</h2>
           <p>${responseJson.items[i].volumeInfo.description}</p>
+          <script type="text/javascript">
+   
+    GBS_insertPreviewButtonPopup('ISBN:0738531367')
+    </script>;
           <a href="${responseJson.items[i].saleInfo.buyLink}">Buy this Book</a>
-          </li>`
+         
+          </li>
+          
+         
+          `
         )};
+        
       $('#search-results').removeClass('hidden');
 };
+
+
 
 function getBooks(searchTerm, titleName, authorName, subjectName, publisherName, isbnNumber, freeBooks, maxResults = 5) {
     
@@ -41,7 +63,7 @@ function getBooks(searchTerm, titleName, authorName, subjectName, publisherName,
         subject: subjectName,
         inpublisher: publisherName,
         isbn: isbnNumber,
-        filter: freeBooks,
+        /*filter: freeBooks,*/
 
         maxResults: maxResults
     };
@@ -61,18 +83,20 @@ function getBooks(searchTerm, titleName, authorName, subjectName, publisherName,
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
-function handleFreeBooks() {
+/*function handleFreeBooks() {
     console.log(`handleFreeBooks ran`);
     if ($('#js-free-only').val() == true) {
         let freeBooks = free-ebooks;
         
     }
-}
+}*/
 function watchForm() {
     console.log(`watchForm ran`);
 $('form').submit (event => {
     event.preventDefault();
-    handleFreeBooks();
+    
+    /*handleFreeBooks();*/
+   
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
     const titleName = $('#js-title-name').val();
@@ -83,7 +107,7 @@ $('form').submit (event => {
 
    
     
-    getBooks(searchTerm, titleName, authorName, subjectName, publisherName, isbnNumber, freeBooks, maxResults);
+    getBooks(searchTerm, titleName, authorName, subjectName, publisherName, isbnNumber, /*freeBooks,*/ maxResults);
 });
 }
 $(watchForm);
