ba1aebd116077ad9af6be8ceab916f9434344f1f
Linden Daniels <lindendaniels@gmail.com>
2019-07-24 13:30:34 -0400

    Implement scroll collapse function

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ const mediaURL = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.
    
 $(document).ready(function() {
     
+
+   /*let handleSearchCollapse =*/ function collapseSearchBar() {
+        console.log(`collapseSearchBar ran`)
+        if (x.matches) {
+        $(window).scroll(function(event) {
+            event.preventDefault();
+            /*$('.explorable-title').css("position", "absolute");
+            $('.explorable-title').css("opacity", "0");
+            $('.search-text').css("position", "absolute");
+            $('.search-text').css("opacity", "0");*/
+            $('.explorable-title').fadeOut();
+            $('.search-text').fadeOut();
+        })
+    }
+}
+           function restoreSearch() {
+               if (x.matches) { 
+            
+             
+            if ($(document.body).scrollTop() == 0 ) {
+                $('.explorable-title').fadeIn();
+                $('.search-text').fadeIn();
+            }
+        
+            }
+        }
+
+    let x = window.matchMedia("(max-width: 700px)")
+    collapseSearchBar(x);
+    restoreSearch(x)
+    x.addListener(collapseSearchBar)
+    x.addListener(restoreSearch)
+    
+    
     function formatQueryParamsBooks(params) {
         console.log(`formatQueryParams ran`);
         const queryItems = Object.keys(params)
